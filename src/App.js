2322c6deae3c0738f203f6da787672cfc96db5fa
ankur <ankur.sh2011@gmail.com>
2021-11-19 10:47:01 +0530

    immages changes

--- src/App.js
+++ src/App.js
@@ -1,46 +1,56 @@
-// import React from 'react';
-// import Navbar from './components/Navbar';
-// import {BrowserRouter as Router,Route } from 'react-router-dom';
-// import {Switch} from 'react-router-dom'
-// // import { BrowserRouter,Route ,Routes } from 'react-router-dom';
-
-// import './App.css';
-
-// function App() {
-//   return (
-//    <>
-//   <Router>
-//     <Navbar/>
-//     <Route>
-//       <Route path ='/' exact/>
-//       </Route>
-    
-
-//   </Router>
-//    </>
-
-//   );
-// }
-
-// export default App;
-
-
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import './App.css';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/services' component={Services} />
+          <Route path='/products' component={Products} />
+          <Route path='/sign-up' component={SignUp} />
+        </Switch>
+      </Router>
+    </>
+  );
+}
 
-
-const App = () => (
-    <div>
-       <BrowserRouter>
-         
-           <div className="container">
-               <Navbar />
-               
-               
-           </div>
-       </BrowserRouter>
-    </div>
-)
 export default App;
+
+
+// import React from 'react';
+// import { BrowserRouter, Route } from 'react-router-dom';
+// import Navbar from './components/Navbar';
+// import './App.css';
+// // import Home from './components/pages/Home';
+// // import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import Services from './components/pages/Services';
+// import Products from './components/pages/Products';
+// import SignUp from './components/pages/SignUp';
+
+
+
+// const App = () => (
+//     <div>
+//        <BrowserRouter>
+         
+//            <div className="container">
+//                <Navbar />
+//                {/* <Route path='/' exact component={Home} /> */}
+//           <Route path='/services' component={Services} />
+//           <Route path='/products' component={Products} />
+//           <Route path='/sign-up' component={SignUp} />
+               
+//            </div>
+//        </BrowserRouter>
+//     </div>
+// )
+// export default App;
